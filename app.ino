2c0fb73304699ac769569ce9da9876a26241c5cf
jannis <jannisko99@gmail.com>
2018-11-13 10:44:26 +0100

    Feeding 8x8 output array (untested)

--- app.ino
+++ app.ino
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 
 static uint8_t nunchuck_buf[6];   // array to store nunchuck data
+static boolean data_arr[8][8]; // array to feed 8x8 LED Matrix
 
 // sends handshake signal to WII Nunchuck
 static void nunchuck_init()
@@ -52,13 +53,13 @@ static int nunchuck_get_data()
 // returns zbutton state 0/1
 static int nunchuck_zbutton()
 {
-  return ((nunchuck_buf[5] >> 0) & 1) ? 0 : 1;  // checks the first bit and returns it
+  return ((nunchuck_buf[5] >> 0) & 0x01) ? 0 : 1;  // checks the first bit and returns it
 }
 
 // returns cbutton state 0/1
 static int nunchuck_cbutton()
 {
-  return ((nunchuck_buf[5] >> 1) & 1) ? 0 : 1;  // checks the second bit and returns it
+  return ((nunchuck_buf[5] >> 1) & 0x01) ? 0 : 1;  // checks the second bit and returns it
 }
 
 // returns value of x-axis joystick
@@ -98,21 +99,31 @@ int loop_cnt = 0;
 byte joyx, joyy, zbut, cbut, accx, accy, accz;
 
 void print_data() {
-  Serial.print("Z Button:  ");
-  Serial.print(zbut);
-  Serial.print("\tC Button:  ");
-  Serial.print(cbut);
-  Serial.print("\tX Joy:  ");
-  Serial.print(map(joyx, 15, 221, 0, 255));
-  Serial.print("\tY Joy:  ");
-  Serial.print(map(joyy, 29, 229, 0, 255));
-  Serial.print("\tX Accel:  ");
-  Serial.print(accx);
-  Serial.print("\tY Accel:  ");
-  Serial.print(accy);
-  Serial.println("\tZ Accel:  ");
-  Serial.print(accz);
-  Serial.print("\n");
+
+  Serial.flush();
+
+  for(int y=0;y<8;y++){
+    for(int x=0;x<8;x++){
+      Serial.print(data_arr[x][y] + " ");
+    }
+    Serial.print("\n");
+  }
+
+  // Serial.print("Z Button:  ");
+  // Serial.print(zbut);
+  // Serial.print("\tC Button:  ");
+  // Serial.print(cbut);
+  // Serial.print("\tX Joy:  ");
+  // Serial.print(map(joyx, 15, 221, 0, 255));
+  // Serial.print("\tY Joy:  ");
+  // Serial.print(map(joyy, 29, 229, 0, 255));
+  // Serial.print("\tX Accel:  ");
+  // Serial.print(accx);
+  // Serial.print("\tY Accel:  ");
+  // Serial.print(accy);
+  // Serial.println("\tZ Accel:  ");
+  // Serial.print(accz);
+  // Serial.print("\n");
 }
 
 void setup() {
@@ -135,9 +146,52 @@ void loop() {
     accy = nunchuck_accely();   //  65 - 173
     accz = nunchuck_accelz();   //  0 - 255
 
+    // map the joy values to 0-2
+    joyx = map(joyx,0,255,0,2);
+    joyy = map(joyy,0,255,0,2);
+
+    // map the accel values to 0-4
+    accx = map(accx,0,1024,0,4);
+    accy = map(accy,0,1024,0,4);
+    accz = map(accz,0,1024,0,4);
+
+    for(int x=0; x<3; x++){
+      for(int y=0; y<3; y++){
+        // fill joystick array
+        // joystick in y direction is inverse
+        data_arr[x][y] = (joyx==x & joyy==map(y,0,2,2,0));
+      }
+    }
+
+    for(int x=0; x<5; x++){
+      for(int y=0; y<5; y++){
+        // fill accel x and y array
+        // acc in y direction is inverse
+        data_arr[x+3][y+3] = (accx==x & accy==map(y,0,4,4,0));
+      }
+    }
+
+    for(int z=0; z<5; z++){
+      // fill accel z row
+      data_arr[z+3][1] = (accz==z);
+    }
+
+    // fill c and z button bits
+    data_arr[1][4] = zbut;
+    data_arr[1][5] = cbut;
+
+    // joy  joy joy 0     0     0     0     0
+    // joy  joy joy accz  accz  accz  accz  accz
+    // joy  joy joy 0     0     0     0     0
+    // 0    0   0   accxy accxy accxy accxy accxy
+    // 0    z   0   accxy accxy accxy accxy accxy
+    // 0    c   0   accxy accxy accxy accxy accxy
+    // 0    0   0   accxy accxy accxy accxy accxy
+    // 0    0   0   accxy accxy accxy accxy accxy
+
     print_data();
   }
   loop_cnt++;
-  delay(100);
+  delay(10);
 
 }
